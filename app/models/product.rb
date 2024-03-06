236fb0eb7c2587d4f8bdef70a0f2eaaae6bd9afc
jk00198 <jk00198@cs6242-vm13.uwg.westga.edu>
2024-03-06 00:00:04 -0500

    Iteration B1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, :price, presence:true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'use a valid image url'
+  }
+  validates :description, length: {minimum: 5, maximum: 80}, allow_blank: true
 end
+ 
