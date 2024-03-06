7619fb9b4db9b29d924a59f553eb90cd737fad2b
jk00198 <jk00198@cs6242-vm13.uwg.westga.edu>
2024-03-06 01:13:23 -0500

    Iteration C4

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav a', minimum: 4
+    assert_select 'h1', "Your Pragmatic Catalog"
+    assert_select 'h2', 2
+    assert_select 'p', 'MyText'
   end
 end
