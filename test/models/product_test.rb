4b45abd335b220699d5e9e217024f8655ba4586c
jk00198 <jk00198@cs6242-vm13.uwg.westga.edu>
2024-03-06 00:14:02 -0500

    Iteration B2

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title:       "test title",
+                          description: "test description",
+                          image_url:   "lorem.jpg")
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "image url" do
+    product = Product.new(title:       "test title",
+                          description: "test description",
+                          image_url:   "lorem.fadsfs")
+    assert product.invalid?
+    assert_equal ["use a valid image url"],
+      product.errors[:image_url]
+  end
+
 end
