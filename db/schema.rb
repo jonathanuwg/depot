dbab68ad94a9b2eff821766b55a007a5dda1d094
jk00198 <jk00198@cs6242-vm13.uwg.westga.edu>
2024-03-06 18:43:07 -0500

    Iteration D1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_03_003536) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_06_233743) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
